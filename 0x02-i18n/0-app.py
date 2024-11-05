9f06d26229695321d39ec968a0f7a8c52f025d7e
biboking-373 <vinnikinyua7@gmail.com>
2024-11-05 10:08:29 +0000

    first

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,6 +1,15 @@
+#!/usr/bin/env python3
+"""A Basic Flask app.
+"""
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
-@app.route("/")
-def home():
-    return render_template("index.html")
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def get_index() -> str:
+    """The home/index page.
+    """
+    return render_template('0-index.html')
